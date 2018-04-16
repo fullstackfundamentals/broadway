097ccbfe194e8efc3e5f3b26021f7e3e38cc156a
Josh Larson <jplhomer@gmail.com>
2018-04-16 17:44:10 -0500

    Add UI

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
     <router-view/>
   </div>
@@ -25,5 +25,13 @@
       color: #42b983;
     }
   }
+}
+
+.shows {
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  max-width: 800px;
+  margin: 0 auto;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>On Broadway...</h1>
+
+    <div class="shows">
+      <div class="show">
+        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/86046-3.jpeg" alt="Phantom of the Opera">
+        <h2>Phantom of the Opera</h2>
+      </div>
+      <div class="show">
+        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/13792-3.jpg" alt="Aladdin">
+        <h2>Aladdin</h2>
+      </div>
+      <div class="show">
+        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/93471-9.jpg" alt="Spongebob Squarepants">
+        <h2>Spongebob Squarepants</h2>
+      </div>
+      <div class="show">
+        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/98366-15.jpg" alt="Waitress">
+        <h2>Waitress</h2>
+      </div>
+      <div class="show">
+        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/98037-15.jpg" alt="Dear Evan Hansen">
+        <h2>Dear Evan Hansen</h2>
+      </div>
+      <div class="show">
+        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/82858-2.jpg" alt="A Bronx Tale">
+        <h2>A Bronx Tale</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>

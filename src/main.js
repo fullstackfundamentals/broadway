87bd2d382566dfee7305f3e0f3984d3d0a7b8262
Josh Larson <jplhomer@gmail.com>
2018-04-17 18:02:42 -0500

    Add firebase config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCNsUqMS5rYpGdZEdSzddUYzN3gnmQ_0_k",
+  authDomain: "broadway-9207f.firebaseapp.com",
+  databaseURL: "https://broadway-9207f.firebaseio.com",
+  projectId: "broadway-9207f",
+  storageBucket: "broadway-9207f.appspot.com",
+  messagingSenderId: "805923264014"
+};
+
+firebase.initializeApp(config);
 
 Vue.config.productionTip = false;
 

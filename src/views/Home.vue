afc80bb08a08c6083dc22dd33dcd7ecf74980554
Josh Larson <jplhomer@gmail.com>
2018-04-18 08:04:53 -0500

    Completely data-driven

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,30 +7,6 @@
         <img :src="show.poster" :alt="show.title">
         <h2>{{ show.title }}</h2>
       </div>
-      <div class="show">
-        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/86046-3.jpeg" alt="Phantom of the Opera">
-        <h2>Phantom of the Opera</h2>
-      </div>
-      <div class="show">
-        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/13792-3.jpg" alt="Aladdin">
-        <h2>Aladdin</h2>
-      </div>
-      <div class="show">
-        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/93471-9.jpg" alt="Spongebob Squarepants">
-        <h2>Spongebob Squarepants</h2>
-      </div>
-      <div class="show">
-        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/98366-15.jpg" alt="Waitress">
-        <h2>Waitress</h2>
-      </div>
-      <div class="show">
-        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/98037-15.jpg" alt="Dear Evan Hansen">
-        <h2>Dear Evan Hansen</h2>
-      </div>
-      <div class="show">
-        <img src="https://d2npu017ljjude.cloudfront.net/images/poster-178275/w230/222222/82858-2.jpg" alt="A Bronx Tale">
-        <h2>A Bronx Tale</h2>
-      </div>
     </div>
   </div>
 </template>

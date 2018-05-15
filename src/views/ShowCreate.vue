e22268a6df7e3aecb231b7f87b7de1ac80b25cfb
Josh Larson <jplhomer@gmail.com>
2018-05-15 09:47:02 -0500

    Persist data

--- src/views/ShowCreate.vue
+++ src/views/ShowCreate.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h1>Create Show</h1>
-    <form>
+    <form @submit.prevent="save">
       <p>
         <label for="title">Title</label>
-        <input type="text" id="title">
+        <input type="text" id="title" v-model="title">
       </p>
       <p>
         <label for="poster">Poster</label>
-        <input type="text" id="poster">
+        <input type="text" id="poster" v-model="poster">
       </p>
       <p>
         <button type="submit">Create</button>
@@ -16,3 +16,29 @@
     </form>
   </div>
 </template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+  data() {
+    return {
+      title: '',
+      poster: ''
+    }
+  },
+  methods: {
+    async save() {
+      const doc = await firebase
+        .firestore()
+        .collection('shows').add({
+          title: this.title,
+          poster: this.poster
+        });
+
+      this.$router.push(`/shows/${doc.id}`);
+    }
+  }
+}
+</script>
+

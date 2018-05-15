8073cb37b34784d17ed8eea2144a60d811afd2a0
Josh Larson <jplhomer@gmail.com>
2018-05-15 09:33:49 -0500

    Add ShowCreate

---
+++ src/views/ShowCreate.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Create Show</h1>
+    <form>
+      <p>
+        <label for="title">Title</label>
+        <input type="text" id="title">
+      </p>
+      <p>
+        <label for="poster">Poster</label>
+        <input type="text" id="poster">
+      </p>
+      <p>
+        <button type="submit">Create</button>
+      </p>
+    </form>
+  </div>
+</template>
